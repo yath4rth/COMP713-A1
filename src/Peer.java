a89f54d63596608c901beb2b89451a252a3e74d3
Yatharth Haldar <yatharthhaldar4007@gmail.com>
2026-09-20 17:07:19 +1200

    Implement two-way peer communication

--- src/Peer.java
+++ src/Peer.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,6 +21,10 @@ public class Peer {
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String message = reader.readLine();
             System.out.println("Message received: "+message);
+
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            writer.println("Hello from Peer 1");
+            System.out.println("Response sent.");
 
         } catch (IOException e) {
             System.out.println("Could not start peer.");
